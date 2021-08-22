dd1fdd9941c65621b1b5ba39e9457e2f03f34d8f
gidwikintan <gidwikintan@gmail.com>
2021-08-23 03:44:43 +0700

    making progress bar using useStorage, firestore

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,4 +1,4 @@
-import * as firebase from 'firebase/app';
+import  firebase from 'firebase/app';
 import 'firebase/storage';
 import "firebase/firestore";
 
@@ -16,6 +16,7 @@ import "firebase/firestore";
 
   const projectStorage = firebase.storage();
   const projectFirestore = firebase.firestore();
+  const timestamp = firebase.firestore.FieldValue.serverTimestamp;
 
 
-  export {projectFirestore, projectStorage};
+  export {projectFirestore, projectStorage, timestamp};
