f78433d9d70bccf4e66361970bbb61a31b213679
gidwikintan <gidwikintan@gmail.com>
2021-08-22 10:54:44 +0700

    intial commit

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import * as firebase from 'firebase/app';
+import 'firebase/storage';
+import "firebase/firestore";
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDYxPjGi4ppHxKLjpprCpf8OryzoPRtzGM",
+    authDomain: "firegram-f99e3.firebaseapp.com",
+    projectId: "firegram-f99e3",
+    storageBucket: "firegram-f99e3.appspot.com",
+    messagingSenderId: "751362288318",
+    appId: "1:751362288318:web:210cdc4c86479e7c394cef"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  const projectStorage = firebase.storage();
+  const projectFirestore = firebase.firestore();
+
+
+  export {projectFirestore, projectStorage};
